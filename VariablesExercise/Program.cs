9ca7f20e2fdef4a37d5d57140061100c4d681a69
Fikr Abebe <fikrabebe24@gmail.com>
2024-03-21 13:59:36 -0400

    Variables Excersise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName="Bingo"; //Declaration syntax
+            int dogAge = 5; //Declartions and Initialization
+            char firstInitial = 'B' ;
+            bool isHungry = true;
+            double dogWeight = 110.7;
+            decimal dogHeight = 2.5m;
+
+
+            Console.WriteLine($"My dog's name is {dogName} and he is {dogAge} years old. His initial is {firstInitial}.");
+
+            Console.ReadLine();
+
+            Console.WriteLine($"He is {dogWeight}lbs and is {dogHeight} feet tall.");
+            Console.WriteLine("and it is frustratingly {isHungry. ToString(). ToLower()} he is always hungry!");
+
         }
     }
 }
